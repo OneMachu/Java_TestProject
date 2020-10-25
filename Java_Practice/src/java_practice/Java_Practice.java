339eabf188bb0569c56a05a08ee950fb6bfa7a99
Juan Matthew Velandres <JuanMatthew.velandres@gmail.com>
2020-10-25 11:24:58 +0800

    First

---
+++ Java_Practice/src/java_practice/Java_Practice.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_practice;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Java_Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+           String Valid;
+           String Type = "asd";
+           
+           System.out.println(Type);
+    }
+    
+}
